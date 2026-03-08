74940d001f2a652ef7ddba12b30da20c75a612e3
Lucioschenkel <lucio.schenkel@proton.me>
2026-03-08 07:51:02 -0300

    feat: exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to rabbitmq at %s", connStr)
+	}
+	defer conn.Close()
+	fmt.Println("Connection successful!")
+
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Failed to create channel: %v", err)
+	}
+
+	pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down..")
 }
