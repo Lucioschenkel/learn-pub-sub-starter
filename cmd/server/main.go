f06db0a6b39ec0fbdf13a1cb75085e11be66eb28
Lucioschenkel <lucio.schenkel@proton.me>
2026-03-12 08:32:20 -0300

    feat: server cmds

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -20,17 +20,49 @@ func main() {
 	defer conn.Close()
 	fmt.Println("Connection successful!")
 
+	// signalChan := make(chan os.Signal, 1)
+	// signal.Notify(signalChan, os.Interrupt)
+	// <-signalChan
+	// fmt.Println("Shutting down..")
+
 	channel, err := conn.Channel()
 	if err != nil {
 		log.Fatalf("Failed to create channel: %v", err)
 	}
 
-	pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
-		IsPaused: true,
-	})
+	gamelogic.PrintServerHelp()
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("Shutting down..")
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		firstWord := words[0]
+		switch firstWord {
+		case "pause":
+			{
+				fmt.Println("Sending pause message...")
+				pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+					IsPaused: true,
+				})
+			}
+		case "resume":
+			{
+				fmt.Println("Sending resume message...")
+				pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+					IsPaused: false,
+				})
+			}
+		case "quit":
+			{
+				fmt.Println("Exiting...")
+				os.Exit(0)
+			}
+		default:
+			fmt.Println("Unknown command")
+		}
+
+	}
+
 }
